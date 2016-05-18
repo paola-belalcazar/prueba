b1f62867fb6750ce9d8faa331f65b00664f48bb1
paola-belalcazar <paoandreabelalcazar@gmail.com>
2016-05-18 17:21:18 -0500

    version 2
    mejora

--- src/prueba/Prueba.java
+++ src/prueba/Prueba.java
@@ -17,6 +17,7 @@ public class Prueba {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("hola");
+        System.out.println("hola paola");
     }
     
 }
